b834f1e3ad19ad2fd8bb382eba689e7e7a9f5b30
Srinivas Mantina <srinivas.mantina@oracle.com>
2020-06-27 10:44:18 +0530

    adding files

---
+++ basics/_1_hello_world.py
@@ -0,0 +1,14 @@
+"""
+This is my first python program
+"""
+
+print("Hello World!")
+
+"""
+Execution Syntax:
+<python_interpreter> <file_name>
+
+Example:
+python3 _1_hello_world.py
+
+"""
---
+++ basics/_2_identifiers.py
@@ -0,0 +1,14 @@
+"""
+A Python identifier is the name used to identify a variable, function, class, module or other object.
+
+Rules of a python identifier:
+    1. Starts with a letter A to Z or a to z or an underscore (_) followed by zero or more letters, underscores and digits.
+    2. Doesn't allow punctuation characters such as @, $, and % within identifiers.
+    3. Case sensitive.
+
+Naming Conventions for Python identifiers:
+    1. Class names start with an uppercase letter. All other identifiers start with a lowercase letter.
+    2. Starting an identifier with a single leading underscore indicates that the identifier is private.
+    3. Starting an identifier with two leading underscores indicates a strongly private identifier.
+    4. Identifier ending with two trailing underscores is a language-defined special name.
+"""
