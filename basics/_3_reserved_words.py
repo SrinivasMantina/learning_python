cd48b6fd9b105616e3dad4dd627fefb57698df3d
Srinivas Mantina <srinivas.mantina@oracle.com>
2020-06-27 10:44:18 +0530

    adding files

---
+++ basics/_3_reserved_words.py
@@ -0,0 +1,19 @@
+"""Reserved words (also called keywords) are predefined in a programming language.
+Reserved words can’t be used as identifiers like name of variable, function etc.
+
+False                  None                    True                   and                    as
+assert                async                    await                  break                 class
+continue             def                        del                     elif                    else
+except               finally                    for                     from                  global
+if                      import                    in                      is                      lambda
+nonlocal             not                        or                      pass                  raise
+return                try                        while                  with                   yield
+
+"""
+
+"""
+List all the keywords
+"""
+import keyword
+
+print(keyword.kwlist)
